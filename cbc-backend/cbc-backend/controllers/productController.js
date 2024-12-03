16935ab1701bcd51cd18bc1ceead4ac570fbbadc
Amesh Manthika <166608158+kgamanthika@users.noreply.github.com>
2024-12-03 13:32:00 +0530

    first commit

---
+++ cbc-backend/cbc-backend/controllers/productController.js
@@ -0,0 +1,34 @@
+import Product  from "../models/product.js";
+import { isAdmin } from "./userController.js";
+
+export function createProduct(req,res){
+
+  if(!isAdmin(req)){
+    res.json({
+      message: "Please login as administrator to add products"
+    })
+    return
+  }
+
+  const newProductData = req.body
+
+  const product = new Product(newProductData)
+
+  product.save().then(()=>{
+    res.json({
+      message: "Product created"
+    })
+  }).catch((error)=>{
+    res.json({
+      message: error
+    })
+  })
+}
+
+export function getProducts(req,res){
+  Product.find({}).then((products)=>{
+    res.json(products)
+  })
+}
+
+
