972063338ff1982e8776edb564c26ddd97024527
Amesh Manthika <166608158+kgamanthika@users.noreply.github.com>
2024-12-03 13:37:11 +0530

    delete incorrect all

--- cbc-backend/cbc-backend/routes/productRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createProduct, getProducts } from '../controllers/productController.js';
-
-const productRouter = express.Router();
-
-productRouter.post("/",createProduct)
-productRouter.get("/",getProducts)
-
-export default productRouter; 
